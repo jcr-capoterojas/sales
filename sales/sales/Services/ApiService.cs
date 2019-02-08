b33387913a36b8bf0dff21ef07035a95f9d44593
jlcapoterojas <capotejl.fmcr12345@gmail.com>
2019-02-08 12:14:39 -0500

    project .NET Core

--- sales/sales/Services/ApiService.cs
+++ sales/sales/Services/ApiService.cs
@@ -9,10 +9,38 @@
     using System.Threading.Tasks;
     using Newtonsoft.Json;
     using Common.Models;
+    using Plugin.Connectivity;
 
     public class ApiService
     {
-     
+
+        public async Task<Response> CheckConnection()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please turn on your internet settings.",
+                };
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            if (!isReachable)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "No internet connection.",
+                };
+            }
+
+            return new Response
+            {
+                IsSuccess = true,
+            };
+        }
+
 
         public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
         {
--- sales/sales/ViewModels/ProductsViewModel.cs
+++ sales/sales/ViewModels/ProductsViewModel.cs
@@ -1,48 +1,89 @@
 ﻿namespace sales.ViewModels
 {
+    using Common.Models;
+    using GalaSoft.MvvmLight.Command;
+    using Services;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using Common.Models;
-    using Services;
+    using System.Windows.Input;
     using Xamarin.Forms;
 
     public class ProductsViewModel : BaseViewModel
     {
+        #region Attributes
+
         private ApiService apiService;
+        private bool isRefreshing;
 
-        private ObservableCollection<Product> product;
+        private ObservableCollection<Product> products;
+        #endregion
 
-        public ObservableCollection<Product> Product
+        #region Properties
+        public ObservableCollection<Product> Products
         {
             get
             {
-               return this.product;
+                return this.products;
             }
             set
             {
-                this.SetValue(ref this.product, value);
+                this.SetValue(ref this.products, value);
             }
         }
 
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+        #endregion
+
+        #region Constructors
         public ProductsViewModel()
         {
             this.apiService = new ApiService();
             this.LoadProducts();
         }
+        #endregion
 
+        #region Methods
         private async void LoadProducts()
         {
+            this.IsRefreshing = false;
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+                return;
+            }
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
             var response = await this.apiService.GetList<Product>(
-                "https://salesapiservice.azurewebsites.net",
+                url,
                 "/api",
                 "/Products");
+
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = true;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
             var list = (List<Product>)response.Result;
-            this.Product =new ObservableCollection<Product>(list);
+            this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
         }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
+        }
+        #endregion
     }
 }
